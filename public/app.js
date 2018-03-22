932bc8bf03afe6ec551e7cd1d4e35f15341097fc
Jared <jhbarnum@gmail.com>
2018-03-22 00:27:35 -0400

    saved

--- public/app.js
+++ public/app.js
@@ -119,3 +119,17 @@ $(document).on("click", "#delete", function () {
 //     .then(function (data) {
 //     });
 // });
+
+$.getJSON("/saved", function (data) {
+  // For each one
+  for (var i = 0; i < data.length; i++) {
+
+    // Display the apropos information on the page
+    $("#saved").append("<p data-id='" + data[i]._id + "'>" +
+      data[i].title + "<br />" +
+      "<a href=" + data[i].link + "> " + data[i].link + "</a> " + "<br />" +
+      "<button class='btn' id='noteBtn' data-id=" + data[i]._id + ">Article Notes</button>" +
+      "<button class='btn' id='delete'  data-id=" + data[i]._id + ">Delete Article</button>" + "</p>");
+  }
+  console.log(data)
+});
